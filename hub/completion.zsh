2986447c14769486cbce6c7c0392c1cbb4b2b447
Taurus Olson <taurusolson@gmail.com>
2012-11-18 17:24:02 +0100

    First commit

---
+++ hub/completion.zsh
@@ -0,0 +1,10 @@
+# Uses git's autocompletion for inner commands. Assumes an install of git's
+# bash `git-completion` script at $completion below (this is where Homebrew
+# tosses it, at least).
+completion=/usr/local/share/zsh/site-functions/_hub
+
+if test -f $completion
+then
+  source $completion
+fi
+
---
+++ zsh/completion.zsh
@@ -0,0 +1,13 @@
+# matches case insensitive for lowercase
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# pasting with tabs doesn't perform completion
+zstyle ':completion:*' insert-tab pending
+
+# Menu for selection
+zstyle ':completion:*:*:*:*:*' menu select
+
+# Bounty killer
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
+
