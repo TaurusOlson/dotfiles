2608c0b5c945daea23c56737683e8267e0607252
Taurus Olson <taurusolson@gmail.com>
2012-11-23 21:47:05 +0100

    Add git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,6 +10,7 @@ fi
 alias gl='git pull --prune'
 alias gst='git status'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gh="git hist"
 alias gp='git push'
 alias gd='git diff'
 alias gci='git commit -m'
