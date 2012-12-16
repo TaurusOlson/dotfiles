5d45ed1d4223030d65215c257db5a1da7e195fc3
Taurus Olson <taurusolson@gmail.com>
2012-12-16 03:16:10 +0100

    Add emacs and lein

---
+++ emacs/aliases.zsh
@@ -0,0 +1,4 @@
+alias emacs="/usr/local/bin/emacsclient -ct"
+alias es="/usr/local/bin/emacs --daemon"
+alias gui-emacs="/Applications/Emacs.app/Contents/MacOs/bin/emacsclient -c -n"
+alias gui-es="/Applications/Emacs.app/Contents/MacOs/Emacs/ --daemon"
