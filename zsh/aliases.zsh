ebeadff8d448ec23e91719c915b2069243f860bd
Taurus Olson <taurusolson@gmail.com>
2013-01-24 22:15:47 +0100

    New aliases and bindkeys

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,12 @@
 alias reload!='. ~/.zshrc'
 alias .zshrc="vim ~/.zshrc"
+
+alias 1='cd -'
+alias 2='cd +2'
+alias 3='cd +3'
+alias 4='cd +4'
+alias 5='cd +5'
+alias 6='cd +6'
+alias 7='cd +7'
+alias 8='cd +8'
+alias 9='cd +9'
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -41,12 +41,24 @@ setopt AUTO_NAME_DIRS
 setopt auto_pushd
 setopt PUSHD_IGNORE_DUPS
 
-zle -N newtab
+bindkey -v
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+# History
+bindkey -M viins '^r' history-incremental-search-backward
+bindkey -M vicmd '^r' history-incremental-search-backward
+
+bindkey '^K' kill-whole-line
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
+bindkey '^F' forward-char
+bindkey '^B' backward-char
+bindkey '^Z' menu-complete
+bindkey '^N' complete-word
+
+autoload edit-command-line
+zle -N edit-command-line
+bindkey '^X^E' edit-command-line
+
+# bindkey '^P' history-beginning-search-backward
+# bindkey '^N' history-beginning-search-forward
+
