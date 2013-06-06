1d2fafaac960674b4e50b4a9d1a67824ddafb69d
Taurus Olson <taurusolson@gmail.com>
2013-06-06 02:27:12 +0200

    Fix alias gui-emacs

--- emacs/aliases.zsh
+++ emacs/aliases.zsh
@@ -1,4 +1,4 @@
 alias emacs="/usr/local/bin/emacsclient -ct"
 alias emacs-daemon="/usr/local/bin/emacs --daemon"
 alias gui-emacs="/Applications/Emacs.app/Contents/MacOs/bin/emacsclient -c -n"
-alias gui-emacs-daemon="/Applications/Emacs.app/Contents/MacOs/Emacs/ --daemon"
+alias gui-emacs-daemon="/Applications/Emacs.app/Contents/MacOS/Emacs --daemon"
