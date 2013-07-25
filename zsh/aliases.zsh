2e41a6f3d9ea4a77bb1f17815b41f2140fb87014
Taurus Olson <taurusolson@gmail.com>
2013-07-25 19:14:56 +0200

    New alias: wh. New function: i()

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,7 @@
 alias reload!='. ~/.zshrc'
 alias .zshrc="vim ~/.zshrc"
 
-# CD {{{1
+# CD  {{{1
 alias 1='cd -'
 alias 2='cd +2'
 alias 3='cd +3'
@@ -13,7 +13,7 @@ alias 8='cd +8'
 alias 9='cd +9'
 alias ..="cd .."
 
-# LS {{{1
+# LS  {{{1
 alias ls="ls -FG"
 alias lsd='ls -l | grep ^d'
 alias ls.='ls -d .*'
@@ -21,6 +21,7 @@ alias ll="ls -lrth"
 alias la="ls -a"
 alias l="dirs -v"
 
-alias sv="source /usr/local/bin/virtualenvwrapper.sh"
+# MISC{{{1
+alias wh=w"hich -a"
 
 # vim:set fdm=marker:
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,1 +1,2 @@
 function mkcd() { mkdir -p "${1}" && cd "${1}"; }
+function i() { (head -n 5; tail -n 5) < "$1" | column -t; }
