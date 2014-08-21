478a0aaea8b4557b36752c9a72618c65d0daf27c
Taurus Olson <taurusolson@gmail.com>
2014-08-21 11:07:37 +0200

    Implement new functions
    - suffix, lines, rename_window, drop

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,2 +1,18 @@
 function mkcd() { mkdir -p "${1}" && cd "${1}"; }
+
 function i() { (head -n 5; tail -n 5) < "$1" | column -t; }
+
+# TODO Allow suffixing of files by taking into account the file extension
+function suffix() { 
+    if [ $# -ne 2 ]; then
+        echo "suffix <dir> <string_to_suffix>"
+    else
+        mv ${1} ${1}_${2}
+    fi
+}
+
+function lines() { wc -l $@; }
+
+function drop() { tail -n +2 $@; }
+
+function rename_window() { echo -en "\e]0;$1\a" }
