038191878095cce661ad69e4724d5868d46ff160
Taurus Olson <taurusolson@gmail.com>
2014-12-12 21:42:13 +0100

    Add aliases for git conflicts

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,4 +18,16 @@ alias gco='git checkout'
 alias gci='git commit'
 alias gcm='git commit -m'
 alias gca='git commit --amend'
+alias gcma='git commit -am'
 alias gconfg="git config --global "
+
+
+# For conflicts
+# Filter all conflicts from git status
+alias conflicts='git status -s | grep -e "^\(DD\|AU\|UD\|UA\|DU\|AA\|UU\)" | cut -d " " -f2'
+
+# Open all conflicts in your favourite editor
+alias resolve='conflicts | xargs $EDITOR'
+
+# Stage them after you've done
+alias cadd='conflicts | xargs git add'
