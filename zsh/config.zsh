c3e43f96aef99ef589082a4f84686278aa3d8165
Taurus Olson <taurusolson@gmail.com>
2015-12-12 13:42:09 +0100

    Don't share history between terminal sessions

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -12,7 +12,7 @@ setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
+# setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt COMPLETE_IN_WORD
@@ -22,7 +22,7 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+setopt INC_APPEND_HISTORY # adds history incrementally
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
@@ -32,7 +32,7 @@ setopt COMPLETE_ALIASES
 
 # Changing making/removing/directory
 setopt AUTO_NAME_DIRS
-setopt auto_pushd
+setopt AUTO_PUSHD
 setopt PUSHD_IGNORE_DUPS
 
 
