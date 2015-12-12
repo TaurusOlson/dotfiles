2c3be733fb3768a7a055f29f5d2d42889694d326
Taurus Olson <taurusolson@gmail.com>
2015-12-12 13:39:52 +0100

    Minor changes

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -19,3 +19,15 @@ function drop() { tail -n +2 $@; }
 function rename_window() { echo -en "\e]0;$1\a" }
 
 function pls() { echo -en $PATH | tr ":" "\n"; }
+
+# Usage: prm <path> [<var>]
+# Remove <path> from PATH or environment variable <var>.
+prm () { eval "${2:-PATH}='$(pls $2 |grep -v "^$1\$" |tr '\n' :)'"; }
+
+# Usage: punshift <path> [<var>]
+# Shift <path> onto the beginning of PATH or environment variable <var>.
+punshift () { eval "${2:-PATH}='$1:$(eval echo \$${2:-PATH})'"; }
+
+# Usage: ppush <path> [<var>]
+ppush () { eval "${2:-PATH}='$(eval echo \$${2:-PATH})':$1"; }
+
