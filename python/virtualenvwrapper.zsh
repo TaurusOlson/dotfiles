31935faae8c208c5e9737c7d39bf18c3db376e80
Taurus Olson <taurusolson@gmail.com>
2018-05-21 01:12:26 +0200

    Add virtualenvwrapper

---
+++ python/virtualenvwrapper.zsh
@@ -0,0 +1,5 @@
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Projects
+export VIRTUALENVWRAPPER_SCRIPT=$HOME/.local/bin/virtualenvwrapper.sh
+source $HOME/.local/bin/virtualenvwrapper.sh
+
